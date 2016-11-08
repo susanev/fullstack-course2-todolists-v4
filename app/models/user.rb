09ff03a0faf7226244cfe257f9f2f0c9a0f806da
susan <susan.ra.evans@gmail.com>
2016-11-08 12:54:05 -0800

    added has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	has_many :todo_lists, dependent: :destroy
 	has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
 	validates :username, presence: true
+	has_secure_password
 
 	def get_completed_count
 		todo_items.to_a.count { |item| item.completed }
