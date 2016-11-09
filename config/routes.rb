119d5341f705948a8dadcd9b9e9f9febed695574
susan <susan.ra.evans@gmail.com>
2016-11-09 12:56:33 -0800

    added a sessions controller with new, create, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items
   end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 end
